01aa036d1ef4b08c640e064c497eeb8051e728fc
Henning Ryll <henning.ryll@web.de>
2023-09-21 13:56:51 +0200

    Update convert.sh
    modified to fit my needs

--- convert.sh
+++ convert.sh
@@ -1,78 +1,136 @@
 #!/bin/bash
 if ! command -v pct &> /dev/null
 then
-    echo "pct could not be found. This script must be run on the host machine proxmox"
-    exit 1
+    echo "pct could not be found. This script must be run on proxmox host"
+#    exit 1
 fi
+
+# set defaults
+name="DietPi-LXC"
+ip="dhcp"
+bridge="vmbr0"
+gateway=""
+port="22"
+#storage="local-lvm"
+storage="local-btrfs"
+rootsize="2"
+memory="2048"
+cores="2"
+password="dietpi"
+
 usage()
 {
     cat <<EOF
-$1 -h|--help
- -n|--name [lxc container name]
- -t|--target [target machine ssh uri]
- -P|--port [target port ssh]
- -i|--id [proxmox container id]
- -s|--root-size [rootfs size in GB]
- -a|--ip [target container ip]
- -b|--bridge [bridge interface]
- -g|--gateway [gateway ip]
- -m|--memory [memory in mb]
- -d|--disk-storage [target proxmox storage pool]
- -p|--password [root password for container (min. 5 chars)]
+
+$1 
+ -h|--help
+ -n|--name [ target lxc container name (default: $name) ]
+ -v|--vm-host [ source virtual machine ssh uri (IP or hostname) ]
+ -P|--port [ ssh port on source virtual machine (default:$port) ]  
+ -i|--id [ target lxc container proxmox container id]
+ -s|--root-size [ target lxc container rootfs size in GB (default: $rootsize GB) ]
+ -I|--ip [ target container ip (default: $ip) ]
+ -b|--bridge [ bridge interface (default: $bridge) ]  
+ -g|--gateway [ gateway ip (default: DHCP) ] 
+ -m|--memory [ target lxc container memory in MB (default: $memory) ]
+ -c|--cores [ target lxc container cpu cores (defalut: $cores) ]
+ -d|--disk-storage [ target proxmox storage pool (default: $storage) ]
+ -p|--password  [root password for container (min. 5 chars) ]
+
 EOF
     return 0
 }
 
-options=$(getopt -o n:t:P:i:s:a:b:g:m:d:p:f -l help,name:,target:,port:,id:,root-size:,ip:,bridge:,gateway:,memory:,disk-storage:,password:,foo: -- "$@")
+usage "$(basename $0)"
+
+options=$(getopt -o n:v:P:i:s:I:b:g:m:c:d:p:f -l help,name:,vm-host:,port:,id:,root-size:,ip:,bridge:,gateway:,memory:,cores:,disk-storage:,password:,foo: -- "$@")
+
 if [ $? -ne 0 ]; then
-        usage "$(basename "$0")"
     exit 1
 fi
 eval set -- "$options"
 
 while true
 do
-    case "$1" in
-        -h|--help)          usage "$0" && exit 0;;
-        -n|--name)          name=$2; shift 2;;
-        -t|--target)        target=$2; shift 2;;
-        -P|--port)          port=$2; shift 2;;
-        -i|--id)            id=$2; shift 2;;
-        -s|--root-size)     rootsize=$2; shift 2;;
-        -a|--ip)            ip=$2; shift 2;;
-        -b|--bridge)        bridge=$2; shift 2;;
-        -g|--gateway)       gateway=$2; shift 2;;
-        -m|--memory)        memory=$2; shift 2;;
-        -p|--password)      password=$2; shift 2;;
-        -d|--disk-storage)  storage=$2; shift 2;;
-        --)                 shift 2; break ;;
-        *)                  break ;;
+    echo "1: $1 || 2: $2"
+	case "$1" in
+        -h|--help)      usage $0 && exit 0
+						;;
+        -n|--name)      name="$2"; shift 2
+						;;
+        -v|--vm-host)   vmhost="$2"; shift 2
+						;;
+        -P|--port)      port="$2"; shift 2
+						;;
+        -i|--id)        id="$2"; shift 2
+						;;
+        -s|--root-size) rootsize="$2"; shift 2
+						;;
+		-I|--ip)        ip="$2/24"; shift 2
+						;;
+        -b|--bridge)    bridge="$2"; shift 2
+						;;
+        -g|--gateway)   gateway=",gw=$2"; shift 2
+						;;
+        -m|--memory)    memory="$2"; shift 2
+						;;
+        -c|--cores)     cores="$2"; shift 2
+						;;
+        -p|--password)  password="$2"; shift 2
+						;;
+        -o|--storage)   storage="$2"; shift 2
+						;;
+        --)             shift 2; break
+						;;
+        *)              break
+						;;
     esac
 done
 
+# Get the next available VMID if not given
+if [ "$id" = "" ]; then
+   id=$(pvesh get /cluster/nextid)
+fi
+
 collectFS() {
     tar -czvvf - -C / \
-	--exclude="sys" \
-	--exclude="dev" \
-	--exclude="run" \
-	--exclude="proc" \
-	--exclude="*.log" \
-	--exclude="*.log*" \
-	--exclude="*.gz" \
-	--exclude="*.sql" \
-	--exclude="swap.img" \
-	.
+        --exclude="sys" \
+        --exclude="dev" \
+        --exclude="run" \
+        --exclude="proc" \
+        --exclude="*.log" \
+        --exclude="*.log*" \
+        --exclude="*.gz" \
+        --exclude="*.sql" \
+        --exclude="swap.img" \
+        .
 }
 
-ssh -p"$port" "root@$target" "$(typeset -f collectFS); collectFS" \
+# remove old file to allow ssh work without errors
+rm -rf "/tmp/$name.tar.gz"
+
+ssh -p"$port" "root@$vmhost" "$(typeset -f collectFS); collectFS" \
     > "/tmp/$name.tar.gz"
 
-pct create "$id" "/tmp/$name.tar.gz" \
-  -description LXC \
-  -hostname "$name" \
-  --features nesting=1 \
-  -memory "$memory" -nameserver 8.8.8.8 \
-  -net0 name=eth0,ip="$ip"/24,gw="$gateway",bridge="$bridge" \
-  --rootfs "$rootsize" -storage "$storage" -password "$password"
+if [ -f /tmp/$name.tar.gz ]; then
+#   cat <<EOF >/tmp/lxc-create.sh
+# #!/bin/bash
+pct create $id "/tmp/$name.tar.gz" \
+ --description "DietPi LXC" \
+ --hostname $name \
+ --features nesting=1 \
+ --memory "$memory" \
+ --cores "$cores" \
+ --net0 name=eth0,ip="$ip",ip6=auto,bridge="$bridge",firewall=1"$gateway" \
+ --rootfs "$rootsize" \
+ --storage "$storage" \
+ --password "$password"
+#EOF
 
-rm -rf "/tmp/$name.tar.gz"
+   if [ $? -eq 0 ]; then
+      rm -rf "/tmp/$name.tar.gz"
+   fi
+
+fi 
+
+exit
